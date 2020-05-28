75d53fc8ec3a02fdde6898ce21e8dd265e4b720b
tritest <36116920+tritest@users.noreply.github.com>
2020-05-28 14:12:02 +0200

    Update themes.sh

--- themes.sh
+++ themes.sh
@@ -7,7 +7,7 @@ cd Themes
 for f in *; do
   if [ -d ${f} ]; then
     cd $f
-    /usr/bin/rcc --binary theme.qrc -o ../../deploy/$f.rcc
+    /usr/bin/rcc --binary theme.qrc -o ../../deploy/$f.rcc --format-version 1
     cd ..
   fi
 done
